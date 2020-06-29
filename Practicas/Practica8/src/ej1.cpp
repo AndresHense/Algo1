d25afbf2317dd4852960e7723e13c6fd1b24aaeb
Andres Hense <andreshense@gmail.com>
2020-06-28 23:23:54 -0300

    redone ej1

--- Practicas/Practica8/src/ej1.cpp
+++ Practicas/Practica8/src/ej1.cpp
@@ -1,16 +1,16 @@
 int ultimo1(vector<int> &v){	
-	return v[v.size()-1];		//4 op
-}										//total 4
+	return v[v.size()-1];		//3 
+}										//t(n)=3 -> O(1)
 
 int ultimo2(vector<int> &v){
-	int i=v.size();				//3 op
-	return v[i-1];					//3 op
-}										//total 6
+	int i=v.size();				//2 
+	return v[i-1];					//3 
+}										//t(n)=5 -> O(1)
 
 int ultimo3(vector<int> &v){	//v.size()=n
-	int i=0;					//2 op
-	while(i<v.size()){	//2 op 
-		i++;					//2 op
-	}							//4n op
-	return v[i-1];			//3 op
-}								//total 4n +5
+	int i=0;						//1
+	while(i<v.size()){		//3, n iteraciones 
+		i++;						//1 
+	}								//t(n)= 3+4n
+	return v[i-1];				//3 
+}									//t(n)= 4n +7 -> O(n)
